f74b8523552b34f1ba7fc8a7ced02c19fdc3f6d4
nechamiToledano <nechami3142@gmail.com>
2025-03-31 23:18:19 +0300

    router

--- pictune/src/App.tsx
+++ pictune/src/App.tsx
@@ -1,5 +1,5 @@
 import type React from "react"
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom"
+import {  Routes, Route, BrowserRouter } from "react-router-dom"
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
