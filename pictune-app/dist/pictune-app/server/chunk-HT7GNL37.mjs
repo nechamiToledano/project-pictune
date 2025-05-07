a856c9b4c35812bf1c7ad2e367e019d0b5b8778f
nechamiToledano <nechami3142@gmail.com>
2025-05-07 14:20:51 +0300

    update not ssr

--- pictune-app/dist/pictune-app/server/chunk-HT7GNL37.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{w as o,x as c}from"./chunk-PS4EPEVH.mjs";import{vd as e,wd as a}from"./chunk-NW7SNNUU.mjs";var l=a("playlists"),P=e(l,c),i=e(l,o),r=e(l,t=>t.selectedPlaylistId),p=e(i,r,(t,s)=>s?t[s]:null),S=e(l,t=>t.loading),x=e(l,t=>t.error);export{P as a,p as b,S as c};
