6fbfacee1f1f1f86664dad8a9c1a75ce843529af
nechamiToledano <nechami3142@gmail.com>
2025-05-11 20:27:42 +0300

    add-hourly-dto

---
+++ pictune-server/PicTune.Core/DTOs/HourlyStatDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicTune.Core.DTOs
+{
+    public class HourlyStatDto
+    {
+        public int Hour { get; set; }
+        public int Count { get; set; }
+    }
+
+}
